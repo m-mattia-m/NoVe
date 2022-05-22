fc639ea9ed39366418370d8c858e6d92a8f82dea
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2022-05-22 15:12:57 +0200

    Update DB-Migration

---
+++ NoVe/Migrations/20220522125656_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NoVe.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AdminVerification",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "KlassenId",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VarificationKey",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VarificationStatus",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdminVerification",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "KlassenId",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "VarificationKey",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "VarificationStatus",
+                table: "User");
+        }
+    }
+}
